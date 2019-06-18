92a78e6f262ee7aac36b24e98240e8ee6e0050cb
patrick-ve <patrick@patrickvaneverdingen.design>
2019-06-19 00:41:41 +0200

    Change MongoDB URI to MongDB Atlas

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: 'mongodb://lovestories:Frogfoot25>@ds239797.mlab.com:39797/heroku_kw34mtnf'}
+    module.exports = {mongoURI: 'mongodb+srv://admin:Frogfoot25@lovestories-qg5ov.mongodb.net/test?retryWrites=true&w=majority'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost:27017/lovestories'}
 }
