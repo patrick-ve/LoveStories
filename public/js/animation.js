73c3f2193825df0d33fd2ebe6ac838e0489c3f67
patrick-ve <patrick@patrickvaneverdingen.design>
2019-06-18 17:03:50 +0200

    Update PUT and DELETE requests

---
+++ public/js/animation.js
@@ -0,0 +1,5 @@
+TweenMax.from(".wrapper__stories-card", 2, {opacity: 0});
+TweenMax.from(".message_error", 2, {opacity: 0});
+TweenMax.from(".message_success", 2, {opacity: 0});
+
+console.log('CHECKED');
