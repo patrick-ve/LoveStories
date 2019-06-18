8b46cb4b9bb4fc95ec81f87efc7952929dd7c532
patrick-ve <patrick@patrickvaneverdingen.design>
2019-06-18 16:18:56 +0200

    Fixed incorrect delete and edit logic

--- routes/stories.js
+++ routes/stories.js
@@ -11,8 +11,10 @@ const Story = mongoose.model('stories');
 // Afhandelen van verhalen
 router.get('/',  ensureAuthenticated, (req, res) => {
     Story.find({})
-        .sort({date: 'desc'})
-        .then(stories => { // Promise waaruit een pagina render volgt
+        .sort({dateAdded: 'desc'})
+
+        // Promise waaruit een pagina render volgt
+        .then(stories => {
             res.render('stories/feed',{
                 stories: stories
             });
@@ -39,6 +41,7 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
             });
         }
     });
+    
 });
 
 router.post('/', ensureAuthenticated, (req, res) => {
@@ -76,13 +79,21 @@ router.put('/:id', ensureAuthenticated, (req, res) => {
 });
 
 router.delete('/:id', ensureAuthenticated, (req, res) => {
-    Story.deleteOne({
+    Story.findOne({
         _id: req.params.id
     })
-        .then(() => {
-            req.flash('success_message', 'Your story has successfully been deleted!')
-            res.redirect('/stories')
-        });
+    .then(story => {
+        if(story.user != req.user.id) {
+            req.flash('error_message', 'You are not allowed to delete this story!');
+            res.redirect('/stories');
+        } else {
+            story.deleteOne()
+                .then(story => {
+                    req.flash('success_message', 'Your story has successfully been deleted!');
+                    res.redirect('/stories');
+                })
+        }
+    });
 });
 
 module.exports = router;
