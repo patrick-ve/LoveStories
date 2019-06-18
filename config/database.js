f16f12dcb6b35f791d31f3bfa40bca36743bb2be
patrick-ve <patrick@patrickvaneverdingen.design>
2019-06-19 00:01:22 +0200

    Changed production database password

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: 'mongodb://lovestories:Stoner63!>@ds139167.mlab.com:39167/heroku_jt5hcdv6'}
+    module.exports = {mongoURI: 'mongodb://lovestories:Frogfoot25>@ds139167.mlab.com:39167/heroku_jt5hcdv6'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost:27017/lovestories'}
 }
