0ffd2f04bdef9810dd5a850dfaadf6d831444667
patrick-ve <patrick@patrickvaneverdingen.design>
2019-06-19 22:10:12 +0200

    Update user model and database URI

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: 'mongodb+srv://admin:Frogfoot25@lovestories-qg5ov.mongodb.net/test?retryWrites=true&w=majority'}
+	module.exports = {mongoURI: 'mongodb+srv://admin:Frogfoot25@lovestories-qg5ov.mongodb.net/test?retryWrites=true&w=majority'};
 } else {
-    module.exports = {mongoURI: 'mongodb://localhost:27017/lovestories'}
+	module.exports = {mongoURI: 'mongodb://localhost:27017/lovestories'};
 }
--- models/User.js
+++ models/User.js
@@ -3,22 +3,23 @@ const Schema = mongoose.Schema;
 
 // Aanmaken van schema voor een story
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        lowercase: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    dateJoined: {
-        type: String
-    }
+	username: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		lowercase: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	// Alhoewel dit een datum aangeeft, wordt een datum als string verkozen. Dit i.v.m. integratie met Moment.js
+	dateJoined: {
+		type: String
+	}
 });
 
 mongoose.model('users', UserSchema);
